ccda7e35a72fb858f5947a2a7475ce4ed6c02235
Kyssta <ammar@ammar.click>
2026-09-10 06:26:00 +0000

    Version 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.kyssta.hermeybeta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.0"
+        versionCode = 5
+        versionName = "0.4.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
